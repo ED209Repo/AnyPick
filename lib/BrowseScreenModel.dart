78bdad246cd0ae0f39db17b2fd17e336e34f153b
SulemanAzeemKhan <142675209+SulemanAzeemKhan@users.noreply.github.com>
2023-09-16 01:07:21 +0500

    Browse-Screen
    Browse-Screen|Browse-Model

---
+++ lib/BrowseScreenModel.dart
@@ -0,0 +1,89 @@
+class browseItem {
+  final String title;
+  final String imageUrl;
+  final String Location;
+  final String time;
+  browseItem({
+    required this.title,
+    required this.imageUrl,
+    required this.Location,
+    required this.time,
+  });
+}
+class BrowseItemModel {
+  static List<browseItem> items = [
+    browseItem(
+      title: 'McDonalds',
+      imageUrl: 'https://unsplash.com/photos/ZuIDLSz3XLg',
+      Location: 'H-10',
+      time: '25 min',
+    ),
+    browseItem(
+      title: 'KFC',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRldj56-OSaTw-EYr1aJBAkKXeF2s_XXbVbUA&usqp=CAU',
+      Location: 'F-10',
+      time: '25 min',
+    ),
+    browseItem(
+      title: 'Hardees',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-gS-GT2kGRuYfS8SOTdpwGpNE9JdzZt2mbA&usqp=CAU',
+      Location: 'F-11',
+      time: '25 min',
+    ),
+    browseItem(
+      title: 'Cafe',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJeWVOiCuEGP_lX_3RLXbOfr8FDT8EIORkYQ&usqp=CAU',
+      Location: 'i-8',
+      time: '25 min',
+    ),
+    browseItem(
+      title: 'Cafe',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJeWVOiCuEGP_lX_3RLXbOfr8FDT8EIORkYQ&usqp=CAU',
+      Location: 'i-8',
+      time: '25 min',
+    ),
+    browseItem(
+      title: 'Cafe',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJeWVOiCuEGP_lX_3RLXbOfr8FDT8EIORkYQ&usqp=CAU',
+      Location: 'i-8',
+      time: '25 min',
+    ),
+    browseItem(
+      title: 'Cafe',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJeWVOiCuEGP_lX_3RLXbOfr8FDT8EIORkYQ&usqp=CAU',
+      Location: 'i-8',
+      time: '25 min',
+    ),
+    browseItem(
+      title: 'Cafe',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJeWVOiCuEGP_lX_3RLXbOfr8FDT8EIORkYQ&usqp=CAU',
+      Location: 'i-8',
+      time: '25 min',
+    ),
+    browseItem(
+      title: 'Cafe',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJeWVOiCuEGP_lX_3RLXbOfr8FDT8EIORkYQ&usqp=CAU',
+      Location: 'i-8',
+      time: '25 min',
+    ),
+    browseItem(
+      title: 'Cafe',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJeWVOiCuEGP_lX_3RLXbOfr8FDT8EIORkYQ&usqp=CAU',
+      Location: 'i-8',
+      time: '25 min',
+    ),
+    browseItem(
+      title: 'Cafe',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJeWVOiCuEGP_lX_3RLXbOfr8FDT8EIORkYQ&usqp=CAU',
+      Location: 'i-8',
+      time: '25 min',
+    ),
+    browseItem(
+      title: 'Cafe',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJeWVOiCuEGP_lX_3RLXbOfr8FDT8EIORkYQ&usqp=CAU',
+      Location: 'i-8',
+      time: '25 min',
+    ),
+    // Add more menu items here
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
+import 'package:anypickdemo/browseScreen.dart';
 import 'package:flutter/material.dart';
-
 import 'SplashScreen.dart';
 void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Splashscreen(),
+      home: BrowseScreen(),
     ),
   );
 }
